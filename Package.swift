e2090f04647df51476970927435eba606a062c6f
KGRACHEV <k.grachev@fitmost.ru>
2025-02-26 14:14:59 +0300

    YandexMapsMobile 4.11.0 full

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsMobile",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "YandexMapsMobile",
+            targets: ["YandexMapsMobile"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "YandexMapsMobile",
+            url: "https://github.com/KonstantinGrachev/YandexMaps/releases/download/4.11.0/YandexMapsMobile.xcframework.zip",
+            checksum: "2c528704bd3206efabf35a4d2dc0c5bc34b3657eba10779e89b81bee5f40eec0"
+        )
+    ]
+)
